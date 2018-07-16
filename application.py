ff2eb1413e5194bf3e5ce6b198125a8e1ce368b5
Hossam <hossam.hosni.95@gmail.com>
2018-07-16 05:42:23 +0200

    Add user registration

--- application.py
+++ application.py
@@ -1,9 +1,10 @@
 import os
 
-from flask import Flask, session
+from flask import Flask, session, render_template, request
 from flask_session import Session
 from sqlalchemy import create_engine
 from sqlalchemy.orm import scoped_session, sessionmaker
+from models import *
 
 app = Flask(__name__)
 
@@ -15,12 +16,30 @@ if not os.getenv("DATABASE_URL"):
 app.config["SESSION_PERMANENT"] = False
 app.config["SESSION_TYPE"] = "filesystem"
 Session(app)
+app.config["SQLALCHEMY_DATABASE_URI"] = os.getenv("DATABASE_URL")
+app.config["SQLALCHEMY_TRACK_MODIFICATIONS"] = False
+db.init_app(app)
 
 # Set up database
-engine = create_engine(os.getenv("DATABASE_URL"))
-db = scoped_session(sessionmaker(bind=engine))
+#engine = create_engine(os.getenv("DATABASE_URL"))
+#db = scoped_session(sessionmaker(bind=engine))
 
 
 @app.route("/")
 def index():
-    return "Project 1: TODO"
+    return render_template("index.html")
+
+@app.route("/signup", methods=["POST"])
+def book():
+
+    # Get form information.
+    name = request.form.get("name")
+    username = request.form.get("username")
+    password = request.form.get("password")
+    email = request.form.get("email")
+
+    # Add user.
+    user = User(name=name, username=username, password=password, email=email)
+    db.session.add(user)
+    db.session.commit()
+    return render_template("index.html")
